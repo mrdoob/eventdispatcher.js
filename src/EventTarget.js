f022ffb488442c7d503d100bea84eef319ca134e
Mr.doob <info@mrdoob.com>
2011-10-26 09:13:53 +0200

    Changed implementation Events were being fired as many times as instances with the previous implementation.

--- src/EventTarget.js
+++ src/EventTarget.js
@@ -2,56 +2,46 @@
  * @author mr.doob / http://mrdoob.com/
  */
 
-EventTarget = function () {};
+EventTarget = function () {
 
-EventTarget.prototype = {
+	var listeners = {};
 
-	constructor: EventTarget,
+	this.addEventListener = function ( type, listener ) {
 
-	listeners: {},
+		if ( listeners[ type ] == undefined ) {
 
-	addEventListener: function ( type, listener ) {
-
-		if ( this.listeners[ type ] == undefined ) {
-
-			this.listeners[ type ] = [];
+			listeners[ type ] = [];
 
 		}
 
-		if ( this.listeners[ type ].indexOf( listener ) === -1 ) {
+		if ( listeners[ type ].indexOf( listener ) === - 1 ) {
 
-			this.listeners[ type ].push( listener );
-
-		}
-
-		return this;
-
-	},
-
-	dispatchEvent: function ( event ) {
-
-		for ( var listener in this.listeners[ event.type ] ) {
-
-			this.listeners[ event.type ][ listener ]( event );
+			listeners[ type ].push( listener );
 
 		}
 
-		return this;
+	};
 
-	},
+	this.dispatchEvent = function ( event ) {
 
-	removeEventListener: function ( type, listener ) {
+		for ( var listener in listeners[ event.type ] ) {
 
-		var index = this.listeners[ type ].indexOf( listener );
+			listeners[ event.type ][ listener ]( event );
+
+		}
+
+	};
+
+	this.removeEventListener = function ( type, listener ) {
+
+		var index = listeners[ type ].indexOf( listener );
 
 		if ( index !== - 1 ) {
 
-			this.listeners[ type ].splice( index, 1 );
+			listeners[ type ].splice( index, 1 );
 
 		}
 
-		return this;
-
-	}
+	};
 
 };
