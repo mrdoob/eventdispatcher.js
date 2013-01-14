a9744bbdf1434f7dec8cc95cad40d51578be9d95
Daniel Robinson <daniel.robinson.open@gmail.com>
2013-01-14 13:52:43 +0000

    allow removal of listeners at dispatch time

--- src/EventDispatcher.js
+++ src/EventDispatcher.js
@@ -28,6 +28,16 @@ var EventDispatcher = function () {
 
 		if ( index !== - 1 ) {
 
+                           if( listeners[ type ].isDispatching ) {
+
+                                   listeners[ type ].dispatchQueueUpdated = true;
+                                   
+                                   listeners[ type ].removedIndexes = listeners[ type ].removedIndexes || [];
+
+                                   listeners[ type ].removedIndexes.push(index);
+
+                           }
+
 			listeners[ type ].splice( index, 1 );
 
 		}
@@ -39,14 +49,38 @@ var EventDispatcher = function () {
 		var listenerArray = listeners[ event.type ];
 
 		if ( listenerArray !== undefined ) {
-			
+
+			listenerArray.isDispatching = true
+
 			event.target = this;
 
 			for ( var i = 0, l = listenerArray.length; i < l; i ++ ) {
 
+                                    if(listenerArray.dispatchQueueUpdated){
+                                        
+                                            l = listenerArray.length;
+
+                                            for(var j = 0, k = listenerArray.removedIndexes.length; j < k; j++){
+
+                                                    if(listenerArray.removedIndexes < i){
+                                                    
+                                                            i--;
+
+                                                    }
+
+                                            }
+
+                                            listenerArray.dispatchQueueUpdated = false;
+
+                                            delete listenerArray.removedIndexes;
+
+                                    }
+
 				listenerArray[ i ].call( this, event );
 
 			}
+ 
+                           listenerArray.isDispatching = false;
 
 		}
 
