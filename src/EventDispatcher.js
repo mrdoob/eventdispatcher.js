56cecb34a06fcd0b45db8f092463a11b9a1a8e0e
flashhawk <flashhawkmx@gmail.com>
2013-08-19 15:24:21 +0800

    Update EventDispatcher.js

--- src/EventDispatcher.js
+++ src/EventDispatcher.js
@@ -1,57 +1,73 @@
-/**
- * @author mrdoob / http://mrdoob.com/
- */
-
-var EventDispatcher = function () {
-
-	var listeners = {};
-
-	this.addEventListener = function ( type, listener ) {
-
-		if ( listeners[ type ] === undefined ) {
-
-			listeners[ type ] = [];
-
-		}
-
-		if ( listeners[ type ].indexOf( listener ) === - 1 ) {
-
-			listeners[ type ].unshift( listener );
-
-		}
-
-	};
-
-	this.removeEventListener = function ( type, listener ) {
-
-		var index = listeners[ type ].indexOf( listener );
-
-		if ( index !== - 1 ) {
-
-			listeners[ type ].splice( index, 1 );
-
-		}
-
-	};
-	
-	this.removeAllEventListeners=function()
+EventDispatcher = function(){};
+EventDispatcher.prototype = 
+{
+	constructor : SPP.EventDispatcher,
+	apply: function ( object ) 
 	{
-
-		listeners ={};
-
-	};
-
-	this.dispatchEvent = function ( event ) {
-
-		var listenerArray = listeners[ event.type ];
-		if ( listenerArray == undefined ) return;
-    		event.target = this;
-   		var i = listenerArray.length;
-  		 while (i-- > 0)
-   		{
-     			listenerArray[i].call(this, event);
-     		}
-
-	};
-
+		object.addEventListener = EventDispatcher.prototype.addEventListener;
+		object.hasEventListener = EventDispatcher.prototype.hasEventListener;
+		object.removeEventListener =EventDispatcher.prototype.removeEventListener;
+		object.dispatchEvent =EventDispatcher.prototype.dispatchEvent;
+		object.on=EventDispatcher.prototype.addEventListener;
+		object.once=EventDispatcher.prototype.once;
+	},
+	addEventListener : function(type, listener)
+	{
+		if (this._listeners === undefined)
+			this._listeners = {};
+		var listeners = this._listeners;
+		if (listeners[type] === undefined)
+		{
+			listeners[type] = [];
+		}
+		if (listeners[type].indexOf(listener) === -1)
+		{
+			listeners[type].push(listener);
+		}
+	},
+	dispatchEvent : function(event)
+	{
+		if (this._listeners === undefined) return;
+		var listeners = this._listeners;
+		var listenerArray = listeners[event.type];
+		if (listenerArray == undefined) return;
+		event.target = this;
+		var array = listenerArray.slice();
+		for ( var i = 0, l = array.length; i < l; i++)
+		{
+			array[i].call(this, event);
+		}
+	},
+	removeEventListener : function(type, listener)
+	{
+		if (this._listeners === undefined)
+			return;
+		var listeners = this._listeners;
+		var index = listeners[type].indexOf(listener);
+		if (index !== -1)
+		{
+			listeners[type].splice(index, 1);
+		}
+	},
+	removeAllEventListeners:function()
+	{
+		if (this._listeners === undefined) return;
+		var listeners = this._listeners;
+		for ( var prop in listeners)
+		{
+			delete listeners[prop];
+		}
+	}
 };
+EventDispatcher.prototype.on=EventDispatcher.prototype.addEventListener;
+EventDispatcher.prototype.once=function(type, listener) 
+{
+	  function g(event) 
+	  {
+		   this.removeEventListener(type, g);
+		   listener.call(this, event);
+	  }
+	  this.addEventListener(type, g);
+};
+
+
