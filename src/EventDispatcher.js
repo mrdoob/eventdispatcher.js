a079f8ba6e3f496beb19779930310c30bb6715b6
Ivo Monti <imonti@ENGStudios.com>
2016-10-14 16:49:37 -0300

    added event handling by priority and the hability to stop propagation

--- src/EventDispatcher.js
+++ src/EventDispatcher.js
@@ -6,9 +6,21 @@ var EventDispatcher = function () {};
 
 Object.assign( EventDispatcher.prototype, {
 
-	addEventListener: function ( type, listener ) {
+	addEventListener: function ( type, listener, priority ) {
 
 		if ( this._listeners === undefined ) this._listeners = {};
+
+		if ( this._propagationAllawed === undefined){
+
+			this._propagationAllawed = true;
+
+			this._stopPropagation = function () {
+
+				this._propagationAllawed = false;
+
+			};
+
+		}
 
 		var listeners = this._listeners;
 
@@ -16,11 +28,27 @@ Object.assign( EventDispatcher.prototype, {
 
 			listeners[ type ] = [];
 
+			listeners[ type ].sortByPriority = function() {
+
+				var byPriority = function( a, b ) {
+
+					return b.priority - a.priority;
+
+				};
+
+				this.sort( byPriority );
+
+			};
+
 		}
 
 		if ( listeners[ type ].indexOf( listener ) === - 1 ) {
 
+			listener.priority = priority ? priority : 0;
+
 			listeners[ type ].push( listener );
+
+			listeners[ type ].sortByPriority();
 
 		}
 
@@ -73,6 +101,7 @@ Object.assign( EventDispatcher.prototype, {
 		if ( listenerArray !== undefined ) {
 
 			event.target = this;
+			event.stopPropagation = this._stopPropagation.bind(this);
 
 			var array = [], i = 0;
 			var length = listenerArray.length;
@@ -85,8 +114,17 @@ Object.assign( EventDispatcher.prototype, {
 
 			for ( i = 0; i < length; i ++ ) {
 
-				array[ i ].call( this, event );
+				if(this._propagationAllawed) {
 
+					array[ i ].call( this, event );
+
+				} else {
+
+					this._propagationAllawed = true;
+
+					break;
+
+				}
 			}
 
 		}
