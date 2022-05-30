fc37627542ad521340ca3a6b10ec74f86afd3215
Windrill <windrill@outlook.com>
2022-05-30 20:27:54 +0800

    Typescript sugar

---
+++ src/EventDispatcher.ts
@@ -0,0 +1,95 @@
+/**
+ * @author mrdoob / http://mrdoob.com/
+ */
+
+interface Event {
+  target: EventDispatcher;
+  type: string;
+  message: string;
+}
+
+type EventLambda = ((event: Event) => any);
+
+class EventDispatcher {
+  public _listeners?: {
+    [index: string]: EventLambda[];
+  };
+
+  addEventListener(type: string, listener: EventLambda) {
+
+    if (this._listeners === undefined) this._listeners = {};
+
+    const listeners = this._listeners;
+
+    if (listeners[type] === undefined) {
+
+      listeners[type] = [];
+
+    }
+
+    if (listeners[type].indexOf(listener) === -1) {
+
+      listeners[type].push(listener);
+
+    }
+
+  }
+
+  hasEventListener(type: string | number, listener: any) {
+
+    if (this._listeners === undefined) return false;
+
+    const listeners = this._listeners;
+
+    return listeners[type] !== undefined && listeners[type].indexOf(listener) !== -1;
+
+  }
+
+  removeEventListener(type: string, listener: EventLambda) {
+
+    if (this._listeners === undefined) return;
+
+    const listeners = this._listeners;
+    const listenerArray = listeners[type];
+
+    if (listenerArray !== undefined) {
+
+      const index = listenerArray.indexOf(listener);
+
+      if (index !== -1) {
+
+        listenerArray.splice(index, 1);
+
+      }
+
+    }
+
+  }
+
+  dispatchEvent(event: Event) {
+
+    if (this._listeners === undefined) return;
+
+    const listeners = this._listeners;
+    const listenerArray = listeners[event.type];
+
+    if (listenerArray !== undefined) {
+
+      event.target = this;
+
+      // Make a copy, in case listeners are removed while iterating.
+      const array = listenerArray.slice(0);
+
+      for (let i = 0, l = array.length; i < l; i++) {
+
+        array[i].call(this, event);
+
+      }
+
+    }
+
+  }
+
+}
+
+export {EventDispatcher};
